2d1cb1197c479295ed75e18dfb73772f76dfa096
KrishnamurthiP <smartkrishna@gmail.com>
2015-09-28 15:45:58 +0530

    This file has been modified based on the feedback.
    This file is version 2 of the original file.

--- UserInfo.java
+++ UserInfo.java
@@ -6,7 +6,7 @@ import java.util.Scanner;			//The First two lines tells about importing required
 class UserInfo					//class definition of UserInfo class begins
  {
 
-   public static void main(String args[])	//main method definition begins
+   public static void main(String args[]) throws java.io.IOException	//main method definition begins
     {
 
       
@@ -15,40 +15,55 @@ class UserInfo					//class definition of UserInfo class begins
 	String name;
 	short age;
 	float height;
+	short choice=1;
 
-
-        System.out.println("\n This program accepts Name, Age, Height from user & prints it. ");
-           System.out.println(" ------------------------------------------------------------- ");
+        System.out.println("\n Program Description: This program accepts Name, Age, Height from \n user & prints it. ");
 
 	Scanner in = new Scanner(System.in);
-
-
-	System.out.println("\n Enter your name: ");
-
-	name=in.nextLine();							//Getting Name
-
-	System.out.println("Enter your age: ");
 	
-	age=in.nextShort();							//Getting Age
-
-	System.out.println("Enter your height: ");
 	
-	height=in.nextFloat();							//Getting Height
+	
+	while(choice==1)							//beginning of while loop
+         {
+
+   
+
+	   System.out.println("\nEnter your name: ");	   
+
+	   name=in.nextLine();							//Getting Name
+
+
+	   System.out.println("Enter your age: ");
+	
+	   age=in.nextShort();							//Getting Age
+
+	   System.out.println("Enter your height: ");
+	
+	   height=in.nextFloat();						//Getting Height
 
         	
-        System.out.println("\n-------------------------------------------------------------- ");
+        
 
-        System.out.println("\n Output: ");					//displaying output
-        System.out.println(" ------- ");
+           System.out.println("\n Output: ");					//displaying output
+           System.out.println(" ------- ");
 
-        System.out.println("\n Your Name: "+name);
-        System.out.println("\n Your Age: "+age);
-	System.out.println("\n Your Height: "+height);
 
-        System.out.println("\n-------------------------------------------------------------- ");
+           System.out.println("\n Your Name: "+name+", age: "+age+", height: "+height);
+
+           System.out.println("\n Would you like to continue ? Press \'1\' to continue or Press any other number to Quit...");
+
+	   choice=in.nextShort();						//accept choice to continue or quit!
+
+	   in.nextLine();   			                               
+
+	   System.out.println(" ------- ");
+
+        
+         }									//end of while loop
 
 
      }	//end of main method
 
 
  }    //end of class UserInfo
+
